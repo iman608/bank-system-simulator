07c77ee48b6c105e2cd9cc244d3b3f4a0c6ca66a
Iman Abedini <iman121006@gmail.com>
2026-02-21 15:27:45 +0330

    Add views.h header file with function declarations

---
+++ include/views.h
@@ -0,0 +1,16 @@
+#ifndef VIEWS_H
+#define VIEWS_H
+
+#include <string>
+
+void clearScreen(void);
+
+void wellcom(void);
+
+int main_menu(void);
+
+int login_menu(std::string /*name of the user*/);
+
+void Goodbye(void);
+
+#endif
